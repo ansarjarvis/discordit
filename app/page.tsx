022322e7c3308797581c0e27f6e32e067b78bc39
Mohammad Ansar <ansar.ofcl00@gmail.com>
2023-08-27 21:20:54 +0530

    feat: add the authentication using clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-semibold">Discordit</h1>
+      <UserButton afterSignOutUrl="/" />
     </>
   );
 }
